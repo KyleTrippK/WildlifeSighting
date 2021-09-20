d760d4d922e03f583f391085cf365e8232e34be0
KyleTrippK <kyle.kibet@student.moringaschool.com>
2021-09-20 11:25:38 +0300

    Adds corresponding tests to the sub-classes

--- src/test/java/models/AnimalsTest.java
+++ src/test/java/models/AnimalsTest.java
@@ -2,16 +2,39 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class AnimalsTest {
 
-    @Before
-    public void setUp() throws Exception {
+//    @Before
+//    public void setUp() throws Exception {
+//    }
+//
+//    @After
+//    public void tearDown() throws Exception {
+//    }
+
+    @Test
+    public void animals_instantiatesCorrectly_true(){
+        Animals testAnimals = testAnimals();
+        assertEquals(true, testAnimals instanceof Animals);
     }
 
-    @After
-    public void tearDown() throws Exception {
+    @Test
+    public void animals_instantiatesWithName_lion(){
+        Animals testAnimals = testAnimals();
+        assertEquals("Lion", testAnimals.getName());
+    }
+
+    @Test
+    public void animals_instantiatesWithId_3(){
+        Animals testAnimals = testAnimals();
+        assertEquals(3, testAnimals.getId());
+    }
+    //helper methods
+    public Animals testAnimals(){
+        return new Animals("Lion", 3);
     }
 }
--- src/test/java/models/EndangeredAnimalsTest.java
+++ src/test/java/models/EndangeredAnimalsTest.java
@@ -2,16 +2,51 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class EndangeredAnimalsTest {
 
-    @Before
-    public void setUp() throws Exception {
+//    @Before
+//    public void setUp() throws Exception {
+//    }
+//
+//    @After
+//    public void tearDown() throws Exception {
+//    }
+    @Test
+    public void endangered_instantiatesCorrectly_true(){
+        EndangeredAnimals testEndangered = testEndangered();
+        assertEquals(true, testEndangered instanceof EndangeredAnimals);
     }
 
-    @After
-    public void tearDown() throws Exception {
+    @Test
+    public void endangered_instantiatesWithName_elephant(){
+        EndangeredAnimals testEndangered = testEndangered();
+        assertEquals("elephant", testEndangered.getEndangeredName());
+    }
+
+    @Test
+    public void endangered_instantiatesWithId_5(){
+        EndangeredAnimals testEndangered = testEndangered();
+        assertEquals(5, testEndangered.getEndangeredId());
+    }
+
+    @Test
+    public void endangered_instantiatesWithHealth_healthy(){
+        EndangeredAnimals testEndangered = testEndangered();
+        assertEquals("healthy", testEndangered.getHealth());
+    }
+
+    @Test
+    public void endangered_instantiatesWithAge_young(){
+        EndangeredAnimals testEndangered = testEndangered();
+        assertEquals("young", testEndangered.getAge());
+    }
+
+    // helper methods
+    public EndangeredAnimals testEndangered(){
+        return new EndangeredAnimals("elephant", 5, "healthy", "young");
     }
 }
